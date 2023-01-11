acf9ec92b686fb52231c4c486edc133faaadb168
Omar Armstead <92233897+OArmstead@users.noreply.github.com>
2023-01-11 13:30:02 -0500

    changes

--- python/fundamentals/fundamentals/for_loop_basic1.py
+++ python/fundamentals/fundamentals/for_loop_basic1.py
@@ -19,13 +19,15 @@
 # If divisible by 10, print "Coding Dojo".
 
 
-# for i in range(1, 101):
-#     print(i)
-#     if i % 10 == 0:
-#         print(str(i)+ ' Coding Dojo')
-#     elif i % 5 == 0:
-#         print(str(i)+ ' Coding')
-
+for i in range(1, 101):
+    if i % 10 == 0:
+        print(str(i)+ ' Coding Dojo')
+    elif i % 5 == 0:
+        print(str(i)+ ' Coding')
+    # needed to add an if statement to not make duplicates if the
+    # number is divisible by 5 and 10.
+    else:
+        print(i)
 
 # Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 
@@ -48,12 +50,11 @@
 #For example, if lowNum=2, highNum=9, and mult=3,
 #the loop should print 3, 6, 9 (on successive lines)
 
-# lowNum = 5
-# highNum = 32
-# multi = 5
+# lowNum = 2
+# highNum = 9
+# multi = 3
 
-# for i in range(lowNum,highNum):
-
+# for i in range(lowNum,highNum + 1):
 #     if i % multi == 0:
 #         print(i)
 
